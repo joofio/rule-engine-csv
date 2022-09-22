bb6196a73baa7f7f94499979db11dbccfab04905
Joao Almeida <joaofilipe90@gmail.com>
2022-09-22 11:12:57 +0100

    genreic route

--- engine_api/views.py
+++ engine_api/views.py
@@ -3,6 +3,7 @@ from flask import jsonify, redirect, render_template, request, session
 import pandas as pd
 from engine_api.rule_engine import apply_rules, read_rules_from_csv, create_decision
 from engine_api import app, auth
+import re
 
 rule0 = read_rules_from_csv("rules.csv")
 # https://blog.miguelgrinberg.com/post/restful-authentication-with-flask
@@ -23,14 +24,19 @@ def redirection():
     return redirect("/apidocs")
 
 
-@app.route("/api/v1/rule0", methods=["POST"])
+@app.route("/api/v1/<rule_nr>", methods=["POST"])
 @auth.login_required
 # @swag_from("docs/logsearch.yml", validation=True)  # validates data automatically
-def apply_rule0():
+def apply_rule0(rule_nr):
     data = request.json["data"]
     # print(data)
     # print(rule0)
-    decision, decision_path = create_decision(data, rule0)
+    print(rule_nr)
+    if bool(re.match("rule\d{1,2}", rule_nr)):
+
+        decision, decision_path = create_decision(data, eval(rule_nr))
+    else:
+        return {"message": "Invalid rule number"}
     return jsonify(
         {
             "decision": decision,
