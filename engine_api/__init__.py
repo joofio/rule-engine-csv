d67b94156df3aad861edc2dfc8342fa527e03520
Joao Almeida <joaofilipe90@gmail.com>
2022-09-27 13:58:43 +0100

    new data

--- engine_api/__init__.py
+++ engine_api/__init__.py
@@ -3,28 +3,12 @@ from flasgger import Swagger, swag_from
 from flask import Flask, jsonify, redirect, render_template, request
 from flask_httpauth import HTTPBasicAuth
 import os
-
+import config
 
 auth = HTTPBasicAuth()
 app = Flask(__name__)
-app.config["SWAGGER"] = {
-    "uiversion": 3,
-    "swagger": "2.0",
-    "info": {
-        "title": "Rule Engine",
-        "description": "Rule Engine API",
-        "contact": {
-            "responsibleOrganization": "Me",
-            "responsibleDeveloper": "Joao Almeida",
-        },
-        "termsOfService": "http://me.com/terms",
-        "version": "0.0.1",
-    },
-    # "host": "http://bibliovigilancia.gim.med.up.pt/",  # overrides localhost:5000
-    "basePath": "api",  # base bash for blueprint registration
-    "schemes": ["http", "https"],
-}
-swagger = Swagger(app, template=app.config["SWAGGER"])
+
+swagger = Swagger(app, template=config.SWAGGER_TEMPLATE)
 
 
 import engine_api.views
